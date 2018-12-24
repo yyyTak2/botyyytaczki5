7278601a05d2b432495a04584e022575f4570c28
Dominik <45369491+yyyTak2@users.noreply.github.com>
2018-12-24 23:46:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('ready', () => {
     bot.user.setStatus('dnd')
     bot.user.setPresence({
         game: {
-            name: '🍌HUEHUEUHIHIHI🍌',
+            name: '🍌💰HUEHUEUHIHIHI💰🍌',
             type: 0
         }
     });
