f5f87d4e442283f361805807221ec42373b1ed61
Dominik <dominik.cz89.com@gmail.com>
2019-06-28 17:33:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,11 @@ var Discord = require('discord.js');
 var bot = new Discord.Client();
 var client = new Discord.Client();
 
-var activities_list = [
-    "with the &help command.", 
-    "with the developers console",
-    "with some code", 
-    "with JavaScript"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
-
+var activevar = ["with the &help command.", "with the developers console", "with some code", "with JavaScript"];
+var activities = activevar[Math.floor(Math.random()*activevar.length)];
 client.on('ready', () => {
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
-});
-
+    client.user.setActivity(activities);
+}
 
 bot.on('message', message => {
 if(message.content.toLowerCase() === '!dsdo')
