eba595e13fb1b4e4ad5b7c0ce49c4fbb161f25fa
Dominik <dominik.cz89.com@gmail.com>
2019-06-28 17:18:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,18 +2,23 @@ var Discord = require('discord.js');
 var bot = new Discord.Client();
 var client = new Discord.Client();
 
-bot.on('ready', () => {
-    bot.user.setStatus('dnd')
-    bot.user.setPresence({
-        game: {
-            name: '🍌 Huehuehue 💰',
-            type: 0
-        }
-    });
-});	
+const activities_list = [
+    "with the &help command.", 
+    "with the developers console",
+    "with some code", 
+    "with JavaScript"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
+});
+
 
 bot.on('message', message => {
-if(message.content.toLowerCase() === '!nitro')
+if(message.content.toLowerCase() === '!dsdo')
    message.channel.send('**Kod niedługo dostępny!**');
     
 });
